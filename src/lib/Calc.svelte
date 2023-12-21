264563492f09daa07d4f755dd5e7ba97fb9ce084
Ben Longden <ben.longden@infinityworks.com>
2023-12-21 13:22:21 +0000

    adds NI and other calculations

--- src/lib/Calc.svelte
+++ src/lib/Calc.svelte
@@ -1,48 +1,49 @@
 <script>
-    export let rates = []
-    export let salaryp = 0
-    let taxes = []
-    export let total = 0
-    export let currency
-    export let hidden = false
+export let rates = {'rates': [], 'tax_free': 0, 'tax_free_taper': 0}
+export let salaryp = 0
+let taxes = []
+export let total = 0
+export let currency
+export let hidden = false
 
-    function calculateTaxableValue(salaryp, taxfreelossp, rate) {
-        var i = { ...rate }
-        if (i.upper !== undefined) {
-            i.upper = Math.min(salaryp, i.upper)
-        } else {
-            i.upper = salaryp
-        }
-
-        if (i.higher) {
-            i.lower = i.lower - taxfreelossp
-        }
-
-        let tax = 0
-        if (i.upper - i.lower > 0) {
-            tax = i.rate/100 * (i.upper - i.lower)
-        }
-        i.tax = tax
-        return i      
+function calculateTaxableValue(salaryp, taxfreelossp, rate) {
+    var i = { ...rate }
+    if (i.upper !== undefined) {
+        i.upper = Math.min(salaryp, i.upper)
+    } else {
+        i.upper = salaryp
     }
 
-
-    let taxfreelossp = 0
-    {
-        if (salaryp > 10000000) {
-            taxfreelossp = (salaryp - 10000000) / 2
-        }
-        total = 0
-        taxes = []
-        rates.forEach(rate => {
-            var i = calculateTaxableValue(salaryp, taxfreelossp, rate)
-            total += i.tax
-            if (i.tax > 0) {
-                taxes.push(i)
-            }
-        })
+    if (i.higher) {
+        i.lower = i.lower - taxfreelossp
     }
 
+    let tax = 0
+    if (i.upper - i.lower > 0) {
+        tax = i.rate/100 * (i.upper - i.lower)
+    }
+    i.tax = tax
+    return i      
+}
+
+
+let taxfreelossp = 0
+if (rates.tax_free_taper && salaryp > rates.tax_free_taper) {
+    taxfreelossp = (salaryp - rates.tax_free_taper) / 2
+    if (taxfreelossp > rates.tax_free) {
+        taxfreelossp = rates.tax_free
+    }
+}
+
+total = 0
+taxes = []
+rates.rates.forEach(rate => {
+    var i = calculateTaxableValue(salaryp, taxfreelossp, rate)
+    total += i.tax
+    if (i.tax > 0) {
+        taxes.push(i)
+    }
+})
 </script>
 {#if hidden == false && total > 0}
 <table style='flex-grow: 1; min-width: 350px; padding: 1em'>
@@ -52,16 +53,20 @@
     <th>Income in bracket</th>
     <th>Tax to pay</th>
 </tr>
+<tr>
+    <td>0%</td>
+    <td>To {currency.format((rates.tax_free - taxfreelossp)/100)}</td>
+    <td>£0.00</td>
+</tr>
 {#each taxes as tax}
 <tr>
     <td>{tax.rate}%</td>
-    <td>{#if tax.upper === undefined || tax.upper < tax.lower}0{:else}{currency.format((tax.upper - tax.lower)/100)}{/if}</td>
+    <td>{#if tax.upper === undefined || tax.upper < tax.lower}0{:else}{currency.format(tax.lower/100)} to {currency.format(tax.upper/100)}{/if}</td>
     <td>{#if tax.tax === undefined}0{:else}{currency.format(tax.tax/100)}{/if}</td>
-    <td></td>
 </tr>
 {/each}
 <tr>
-    <td></td>
+    <td>Total</td>
     <td></td>
     <td>{currency.format(total/100)}</td>
 </tr>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,62 +1,133 @@
 <script>
     import Calc from '$lib/Calc.svelte'
-    let scotland_year2425 = [
-        {
-            'rate': 19,
-            'lower': 1257000,
-            'upper': 1487600
-        },
-        {
-            'rate': 20,
-            'lower': 1487600,
-            'upper': 2656100
-        },
-        {
-            'rate': 21,
-            'lower': 2656100,
-            'upper': 4366200
-        },
-        {
-            'rate': 42,
-            'lower': 4366200,
-            'upper': 7500000
-        },
-        {
-            'rate': 45,
-            'lower': 7500000,
-            'upper': 12514000,
-            'higher': true
-        },
-        {
-            'lower': 12514000,
-            'rate': 48
-        }
-    ]
+    let uk_ni_2425 = {
+        'tax_free': 1257000,
+        'rates': [
+            {
+                'rate': 10,
+                'lower': 1257000,
+                'upper': 5026800
+            },
+            {
+                'rate': 2,
+                'lower': 5026800,
+            }
+        ]
+    }
+    let uk_ni_2324 = {
+        'tax_free': 1257000,
+        'rates': [
+            {
+                'rate': 12,
+                'lower': 1257000,
+                'upper': 5026800
+            },
+            {
+                'rate': 2,
+                'lower': 5026800,
+            }
+        ]
+    }
+    let scotland_tax_2324 = {
+        'tax_free': 1257000,
+        'tax_free_taper': 10000000,
+        'rates': [
+            {
+                'rate': 19,
+                'lower': 1257000,
+                'upper': 1473200
+            },
+            {
+                'rate': 20,
+                'lower': 1473200,
+                'upper': 2568800
+            },
+            {
+                'rate': 21,
+                'lower': 2568800,
+                'upper': 4366200
+            },
+            {
+                'rate': 42,
+                'lower': 4366200,
+                'upper': 12514000,
+                'higher': true
+            },
+            {
+                'lower': 12514000,
+                'rate': 47
+            }
+        ]
+    }
+    let scotland_tax_2425 = {
+        'tax_free': 12570,
+        'tax_free_taper': 10000000,
+        'rates': [
+            {
+                'rate': 19,
+                'lower': 1257000,
+                'upper': 1487600
+            },
+            {
+                'rate': 20,
+                'lower': 1487600,
+                'upper': 2656100
+            },
+            {
+                'rate': 21,
+                'lower': 2656100,
+                'upper': 4366200
+            },
+            {
+                'rate': 42,
+                'lower': 4366200,
+                'upper': 7500000
+            },
+            {
+                'rate': 45,
+                'lower': 7500000,
+                'upper': 12514000,
+                'higher': true
+            },
+            {
+                'lower': 12514000,
+                'rate': 48
+            }
+        ]
+    }
 
-    let uk_year2425 = [
-        {
-            'rate': 20,
-            'lower': 1257000,
-            'upper': 5027000
-        },
-        {
-            'rate': 40,
-            'lower': 5027000,
-            'upper': 12514000,
-            'higher': true
-        },
-        {
-            'rate': 45,
-            'lower': 12514000
-        },
-    ]
+    let uk_tax_2425 = {
+        'tax_free': 1257000,
+        'tax_free_taper': 10000000,
+        'rates': [
+            {
+                'rate': 20,
+                'lower': 1257000,
+                'upper': 5027000
+            },
+            {
+                'rate': 40,
+                'lower': 5027000,
+                'upper': 12514000,
+                'higher': true
+            },
+            {
+                'rate': 45,
+                'lower': 12514000
+            }
+        ]
+    }
 
     let salary = 0
     let salary_t = 0
 
-    let scotland_total 
-    let uk_total
-    let onepc_scotland_total
+    let scotland_tax_2425_total 
+    let scotland_tax_2324_total 
+    let uk_tax_2425_total
+    let scotland_tax_1pc_2425_total
+    let uk_ni_2425_total
+    let uk_ni_2324_total
+    let uk_ni_1pc_2425_total
 
     let GBP = new Intl.NumberFormat('en-GB', {
         style: 'currency',
@@ -67,51 +138,58 @@
         salary = salary_t
     }
 
+    function fc(valuep) {
+        return GBP.format(Math.abs(valuep/100))
+    }
 </script>
-<h2>Tax Calculator 24/25</h2>
 <div class="container">
+    <h1>Tax Calculator 24/25</h1>
     <label for="salary">
         Salary
         <input id="salary" bind:value={salary_t}/>
     </label>
-    <button style='width: 250px'on:click={setSalary}>Calculate</button>
+    <button on:click={setSalary}>Calculate</button>
 </div>
-
 <div class="container">
+{#if (salary > 0)} 
     <p>
-        {#if scotland_total - uk_total > 0}
-            You will pay {GBP.format((scotland_total - uk_total)/100)} more tax to live in in Scotland then you would
-            if you lived elsewhere in the UK.
-        {:else if uk_total - scotland_total > 0}
-            You will pay {GBP.format((uk_total - scotland_total)/100)} less tax to live in Scotland then you would
-            if you lived elsewhere in the UK.
+        {#if scotland_tax_2425_total > 0}
+            You will pay {fc(scotland_tax_2425_total - uk_tax_2425_total)} {#if scotland_tax_2425_total - uk_tax_2425_total > 0}more{:else}less{/if} tax
+            to live in Scotland then you would if you lived elsewhere in the UK. You will pay 
+            {fc(scotland_tax_2425_total - scotland_tax_2324_total)} {#if (scotland_tax_2425_total > scotland_tax_2324_total)}more{:else}less{/if}
+            tax than last year, though if you have had a pay rise your tax will rise accordingly.
         {:else}
             Your income is under the current tax free allowance. You will not pay tax in Scotland or in the rest of the UK.
         {/if}
     </p>
 
-{#if salary > 100000}
-<p>
-    Your salary is over £100,000. For every £2 you earn over £100k you lose £1 of your tax free allowance, which
-    is then taxed at the higher rate (resulting in a marginal rate of up to 68%). You can use salary sacrifice or
-    increase pension contributions to keep your taxable salary at under £100,000 so you keep more of your money at a
-    relatively low cost.
-</p>
+    <!-- {#if salary > 100000}
+    <p>
+        Your salary is over £100,000. For every £2 you earn over £100k you lose £1 of your tax free allowance, which
+        is then taxed at the higher rate (resulting in a marginal rate of up to 68%) until you earn £125,140.
+    </p>
+    {/if} -->
+
+    <p>
+        You will pay {fc(uk_ni_2425_total)} in National Insurance, {fc(uk_ni_2425_total - uk_ni_2324_total)} {#if uk_ni_2425_total - uk_ni_2324_total > 0}more{:else}less{/if}
+        than last year.
+    </p>
+
+    <p>
+        A 1% increase in salary sacrifice or pension contribution would be worth {fc(salary)}, save you {fc(scotland_tax_2425_total - scotland_tax_1pc_2425_total)}
+        in tax, {fc(uk_ni_2425_total - uk_ni_1pc_2425_total)} in National Insurance, and cost
+        {fc((salary)-(scotland_tax_2425_total - scotland_tax_1pc_2425_total)-(uk_ni_2425_total - uk_ni_1pc_2425_total))} from your net salary.
+    </p>
+
+    {#key salary}
+    <Calc bind:total={scotland_tax_1pc_2425_total} rates={scotland_tax_2425} salaryp={salary * 99} currency={GBP} hidden=true>99% Scotland</Calc>
+    <Calc bind:total={scotland_tax_2425_total} rates={scotland_tax_2425} salaryp={salary * 100} currency={GBP}>Scotland 24/25</Calc>
+    <Calc bind:total={scotland_tax_2324_total} rates={scotland_tax_2324} salaryp={salary * 100} currency={GBP}>Scotland 23/24</Calc>
+    <Calc bind:total={uk_tax_2425_total} rates={uk_tax_2425} salaryp={salary * 100} currency={GBP}>UK 24/25</Calc>
+    <Calc bind:total={uk_ni_2425_total} rates={uk_ni_2425} salaryp={salary * 100} currency={GBP}>UK National Insurance 24/25</Calc>
+    <Calc bind:total={uk_ni_1pc_2425_total} rates={uk_ni_2425} salaryp={salary * 99} currency={GBP} hidden=true>99% UK National Insurance 24/25</Calc>
+    <Calc bind:total={uk_ni_2324_total} rates={uk_ni_2324} salaryp={salary * 100} currency={GBP}>UK National Insurance 23/24</Calc>
+    {/key}
 {/if}
-
-
-<p>
-    A 1% increase in salary sacrifice or pension contribution would save you {GBP.format((scotland_total - onepc_scotland_total)/100)}
-    in tax, and cost {GBP.format(salary * 0.01)} ({GBP.format((salary * 0.01)-(scotland_total - onepc_scotland_total)/100)} net)
-</p>
+    <p>Made in 🏴󠁧󠁢󠁳󠁣󠁴󠁿 by <a href="https://x.com/blongden">@blongden</a></p>
 </div>
-
-{#key salary}
-<div class="container" style='display: flex; flex-wrap: wrap;'>
-<Calc bind:total={onepc_scotland_total} rates={scotland_year2425} salaryp={salary * 99} currency={GBP} hidden=true>99% Scotland</Calc>
-<Calc bind:total={scotland_total} rates={scotland_year2425} salaryp={salary * 100} currency={GBP}>Scotland</Calc>
-<Calc bind:total={uk_total} rates={uk_year2425} salaryp={salary * 100} currency={GBP}>UK</Calc>
-</div>
-{/key}
-
-<p>Made in 🏴󠁧󠁢󠁳󠁣󠁴󠁿 by <a href="https://x.com/blongden">@blongden</a></p>
